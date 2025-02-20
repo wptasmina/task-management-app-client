d9027f0a3346464c6ca982ec9f5ef4a396c80388
wptasmina <wptasmina@gmail.com>
2025-02-20 13:26:21 +0600

    first commit setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl font-bold underline bg-red-400">
+          Hello world!
+        </h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
